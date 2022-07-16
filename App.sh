5076ff334f97cd9d1d7ee9d4aca7bb6e75a9104a
Samer Saeid <74488921+sameralsayed@users.noreply.github.com>
2022-07-16 07:53:44 -0700

    Update App.sh

--- App.sh
+++ App.sh
@@ -1,1 +1,42 @@
+#!/bin/bash
 
+    #Name : Twitter Card Generator
+    #Created by : SAMER SAEID
+    #Copyright : MIT
+    #URL: https://www.facebook.com/samerthehariri/
+    #Date: 11/19/2021
+
+echo "Site Name"
+read SiteName
+
+echo "Site Username:"
+read SiteUsername
+
+echo "App Name:"
+read AppName
+
+echo "iPhone App ID:"
+read iPhoneApp
+
+echo "iPad App ID:"
+read iPadApp
+
+echo "Google Play App ID:"
+read GooglePlay
+
+echo "App Country (If Not Available in US Store):"
+read AppCountry
+
+echo "Description (Characters left: 194):"
+read Description
+
+echo "<meta name=\"twitter:card\" content=\"app\">" >> ${SiteName}.txt
+echo "<meta name=\"twitter:site\" content=\"@${SiteUsername}\">" >> ${SiteName}.txt
+echo "<meta name=\"twitter:app:name:iphone\" content=\"${AppName}\">" >> ${SiteName}.txt
+echo "<meta name=\"twitter:app:name:ipad\" content=\"${AppName}\">" >> ${SiteName}.txt
+echo "<meta name=\"twitter:app:name:googleplay\" content=\"${AppName}\">" >> ${SiteName}.txt
+echo "<meta name=\"twitter:app:id:iphone\" content=\"${iPhoneApp}\">" >> ${SiteName}.txt
+echo "<meta name=\"twitter:app:id:ipad\" content=\"${iPadApp}\">" >> ${SiteName}.txt
+echo "<meta name=\"twitter:app:id:googleplay\" content=\"${GooglePlay}\">" >> ${SiteName}.txt
+echo "<meta name=\"twitter:app:country\" content=\"${AppCountry}\">" >> ${SiteName}.txt
+echo "<meta name=\"twitter:description\" content=\"${Description}\">" >> ${SiteName}.txt
